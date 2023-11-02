5063c5a2c75591ee9e5c384c2e6897e1b38c04fa
Jonas Abreu <jvdsa1893@gmail.com>
2023-11-02 10:58:07 -0300

    Testand a primeira classe de dominio - Categoria

--- src/main/java/com/jonas/resources/CategoriaResource.java
+++ src/main/java/com/jonas/resources/CategoriaResource.java
@@ -1,44 +1,30 @@
 package com.jonas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jonas.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-	
-	int id;
-	String nome;
-	
-	public CategoriaResource() {
-	}
 
-	public CategoriaResource(int id, String nome) {
-		super();
-		this.id = id;
-		this.nome = nome;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> lista() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
